4cd23542b96edca78b7db0681ef62aaf4311822e
lamnhan066 <hello@lamnhan.dev>
2025-04-09 22:35:41 +0700

    Refactor CloudSyncHiveAdapter constructor to use named parameters and change detailBox to LazyBox

--- packages/cloud_sync_hive_adapter/lib/src/cloud_sync_hive_adapter.dart
+++ packages/cloud_sync_hive_adapter/lib/src/cloud_sync_hive_adapter.dart
@@ -5,13 +5,16 @@ import 'package:hive_ce/hive.dart';
 /// This adapter is responsible for syncing metadata and notes.
 class CloudSyncHiveAdapter implements SyncAdapter<SyncMetadata, String> {
   /// Creates a [CloudSyncHiveAdapter] with the given Hive boxes for notes and metadata.
-  const CloudSyncHiveAdapter(this.metadataBox, this.detailBox);
+  const CloudSyncHiveAdapter({
+    required this.metadataBox,
+    required this.detailBox,
+  });
 
   /// The Hive box used to store metadata.
   final Box<SyncMetadata> metadataBox;
 
   /// The Hive box used to store notes.
-  final Box<String> detailBox;
+  final LazyBox<String> detailBox;
 
   /// Fetches the list of metadata from the metadata Hive box.
   ///
@@ -29,7 +32,7 @@ class CloudSyncHiveAdapter implements SyncAdapter<SyncMetadata, String> {
   /// Returns the note content as a string.
   @override
   Future<String> fetchDetail(SyncMetadata metadata) async {
-    return detailBox.get(metadata.id)!;
+    return (await detailBox.get(metadata.id))!;
   }
 
   /// Saves the given metadata and note content to their respective Hive boxes.
