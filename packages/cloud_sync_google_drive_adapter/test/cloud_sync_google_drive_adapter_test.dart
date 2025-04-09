c14c688ce293a7b5553fe0f97ca11e4390dd7bee
lamnhan066 <hello@lamnhan.dev>
2025-04-09 22:35:06 +0700

    Add unit tests for CloudSyncGoogleDriveAdapter methods

--- packages/cloud_sync_google_drive_adapter/test/cloud_sync_google_drive_adapter_test.dart
+++ packages/cloud_sync_google_drive_adapter/test/cloud_sync_google_drive_adapter_test.dart
@@ -1,1 +1,164 @@
+import 'dart:typed_data';
 
+import 'package:cloud_sync/cloud_sync.dart';
+import 'package:cloud_sync_google_drive_adapter/cloud_sync_google_drive_adapter.dart';
+import 'package:googleapis/drive/v3.dart' as drive;
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+// === Mocks ===
+class MockDriveApi extends Mock implements drive.DriveApi {}
+
+class MockFilesResource extends Mock implements drive.FilesResource {}
+
+class MockGeneratedIds extends Mock implements drive.GeneratedIds {}
+
+class MockFileList extends Mock implements drive.FileList {}
+
+class MockDriveFile extends Mock implements drive.File {}
+
+class MockMedia extends Mock implements drive.Media {}
+
+class FakeDriveFile extends Fake implements drive.File {}
+
+void main() {
+  late MockDriveApi mockDriveApi;
+  late MockFilesResource mockFiles;
+  late CloudSyncGoogleDriveAdapter adapter;
+
+  final testMetadata = SyncMetadata(id: 'test-id', modifiedAt: DateTime.now());
+  final jsonDescription = testMetadata.toJson();
+
+  setUpAll(() {
+    registerFallbackValue(FakeDriveFile());
+  });
+
+  setUp(() {
+    mockDriveApi = MockDriveApi();
+    mockFiles = MockFilesResource();
+
+    when(() => mockDriveApi.files).thenReturn(mockFiles);
+    adapter = CloudSyncGoogleDriveAdapter(driveApi: mockDriveApi);
+  });
+
+  test('generateIds returns list of IDs', () async {
+    final mockIds = MockGeneratedIds();
+    when(() => mockIds.ids).thenReturn(['abc', 'def']);
+    when(
+      () => mockFiles.generateIds(count: 2),
+    ).thenAnswer((_) async => mockIds);
+
+    final result = await adapter.generateIds(2);
+
+    expect(result, ['abc', 'def']);
+    verify(() => mockFiles.generateIds(count: 2)).called(1);
+  });
+
+  test('fetchMetadataList returns list of SyncMetadata', () async {
+    final mockFileList = MockFileList();
+    final file =
+        drive.File()
+          ..id = testMetadata.id
+          ..description = jsonDescription
+          ..modifiedTime = testMetadata.modifiedAt;
+
+    when(() => mockFileList.files).thenReturn([file]);
+    when(() => mockFileList.nextPageToken).thenReturn(null);
+    when(
+      () => mockFiles.list(
+        spaces: any(named: 'spaces'),
+        pageToken: any(named: 'pageToken'),
+        $fields: any(named: '\$fields'),
+        q: any(named: 'q'),
+      ),
+    ).thenAnswer((_) async => mockFileList);
+
+    final metadataList = await adapter.fetchMetadataList();
+
+    expect(metadataList.length, 1);
+    expect(metadataList.first.id, testMetadata.id);
+  });
+
+  test('fetchDetail returns list of bytes from Media stream', () async {
+    final mockMedia = MockMedia();
+    final fileBytes = [
+      Uint8List.fromList([1, 2]),
+      Uint8List.fromList([3]),
+    ];
+
+    when(
+      () => mockMedia.stream,
+    ).thenAnswer((_) => Stream.fromIterable(fileBytes));
+    when(
+      () => mockFiles.get(
+        testMetadata.id,
+        downloadOptions: drive.DownloadOptions.fullMedia,
+      ),
+    ).thenAnswer((_) async => mockMedia);
+
+    final bytes = await adapter.fetchDetail(testMetadata);
+
+    expect(bytes, [1, 2, 3]);
+  });
+
+  test('save creates file if metadata not found', () async {
+    final mockFileList = MockFileList();
+    when(() => mockFileList.files).thenReturn([]);
+    when(() => mockFileList.nextPageToken).thenReturn(null);
+    when(
+      () => mockFiles.list(
+        spaces: any(named: 'spaces'),
+        pageToken: any(named: 'pageToken'),
+        $fields: any(named: '\$fields'),
+        q: any(named: 'q'),
+      ),
+    ).thenAnswer((_) async => mockFileList);
+
+    when(
+      () => mockFiles.create(any(), uploadMedia: any(named: 'uploadMedia')),
+    ).thenAnswer((_) async => drive.File());
+
+    await adapter.save(testMetadata, [1, 2, 3]);
+
+    verify(
+      () => mockFiles.create(any(), uploadMedia: any(named: 'uploadMedia')),
+    ).called(1);
+  });
+
+  test('save updates file if metadata found', () async {
+    final mockFileList = MockFileList();
+    final driveFile =
+        drive.File()
+          ..id = testMetadata.id
+          ..description = jsonDescription;
+
+    when(() => mockFileList.files).thenReturn([driveFile]);
+    when(() => mockFileList.nextPageToken).thenReturn(null);
+    when(
+      () => mockFiles.list(
+        spaces: any(named: 'spaces'),
+        pageToken: any(named: 'pageToken'),
+        $fields: any(named: '\$fields'),
+        q: any(named: 'q'),
+      ),
+    ).thenAnswer((_) async => mockFileList);
+
+    when(
+      () => mockFiles.update(
+        any(),
+        testMetadata.id,
+        uploadMedia: any(named: 'uploadMedia'),
+      ),
+    ).thenAnswer((_) async => drive.File());
+
+    await adapter.save(testMetadata, [4, 5, 6]);
+
+    verify(
+      () => mockFiles.update(
+        any(),
+        testMetadata.id,
+        uploadMedia: any(named: 'uploadMedia'),
+      ),
+    ).called(1);
+  });
+}
